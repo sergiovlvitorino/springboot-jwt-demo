8f5f0e1a89bd5112611ecf31f8505f2de984b790
Sergio Vitorino <sergiovlvitorino@gmail.com>
2019-10-14 14:09:02 -0300

    Refactoring

--- src/main/java/com/sergiovitorino/springbootjwt/Start.java
+++ src/main/java/com/sergiovitorino/springbootjwt/Start.java
@@ -6,12 +6,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-<<<<<<< HEAD
-@EnableAspectJAutoProxy
-@SpringBootApplication
-=======
 @EnableWebMvc
->>>>>>> b53c957fa28e113df9141914607c146cd922224b
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class Start {
